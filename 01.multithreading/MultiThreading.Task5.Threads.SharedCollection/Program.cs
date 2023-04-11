f505a5b3aeb3059d12b745f0f5f5c73968cb6a95
Sona_Arshakyan <Sona_Arshakyan@EPAMYERW02F5>
2023-04-11 19:14:53 +0400

    commit was done for task 5

--- 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,18 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        private readonly static int elementsCount = 10;
+        private readonly static List<int> elementsCollection = new List<int>();
+        static Mutex mutex = new Mutex();
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -17,9 +24,41 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+            for (int i = 1; i <= elementsCount; i++)
+            {
+                ThreadPool.QueueUserWorkItem(DoWork, i);
+                Task task = Task.Factory.StartNew(DoWork, null);
+                task.Wait();
+            }
+            Task.WaitAll();
+        }
 
-            Console.ReadLine();
+        private static void DoWork(object state)
+        {
+            // Acquire the mutex
+            mutex.WaitOne();
+
+            Console.WriteLine();
+
+            if (state != null)
+            {
+                Console.WriteLine("Thread one Acquired mutex");
+                int elementValue = (int)state;
+                elementsCollection.Add(elementValue);
+                Console.WriteLine("Thread one added an element");
+            }
+            else
+            {
+                Console.WriteLine("Thread second Acquired mutex");
+                foreach (var element in elementsCollection)
+                {
+                    Console.Write(element);
+                }
+            }
+
+            mutex.ReleaseMutex();
+            // Release the mutex
+            Console.WriteLine();
         }
     }
 }
