6937dc6c09979440f7cc62a6ac36bb9384b7e5f1
Sona_Arshakyan <Sona_Arshakyan@EPAMYERW02F5>
2023-04-30 23:36:28 +0400

    solution sending doc files and receiving using service bus

---
+++  Message Queues/Message Queues/Data Capture Service/Program.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Messaging.ServiceBus;
+
+
+string serviceBusConnectionString = "my service bus conn string";
+string serviceBusTopicName = "captureddatamessages-topic";
+
+// fullPath=bin\Debug\net6.0\Data Folder To Send
+string fullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Data Folder To Send");
+string[] docFilePaths = Directory.GetFiles(fullPath, "*.docx");
+
+await using var client = new ServiceBusClient(serviceBusConnectionString);
+ServiceBusSender sender = client.CreateSender(serviceBusTopicName);
+
+try
+{
+    int messageCount = 0;
+
+    foreach (string docFilePath in docFilePaths)
+    {
+        messageCount++;
+
+        byte[] docBytes;
+        using (var fileStream = new FileStream(docFilePath, FileMode.Open, FileAccess.Read))
+        {
+            using var memoryStream = new MemoryStream();
+            fileStream.CopyTo(memoryStream);
+            docBytes = memoryStream.ToArray();
+        }
+        Console.WriteLine($"Sending messages: {messageCount}");
+
+        var message = new ServiceBusMessage(docBytes);
+        await sender.SendMessageAsync(message);
+    }
+}
+catch (Exception exception)
+{
+    Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+    Console.WriteLine();
+    Console.WriteLine("Messages are sent");
+
+}
+
+
---
+++  Message Queues/Message Queues/Processing Service/Program.cs
@@ -0,0 +1,65 @@
+﻿using Azure.Messaging.ServiceBus;
+
+internal class Program
+{
+    private static readonly string serviceBusConnectionString = "my service bus conn string";
+    private static readonly string serviceBusTopicName = "captureddatamessages-topic";
+    private static readonly string serviceBusSubName = "processingDataMessages-sub";
+    private static readonly string directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @$"Data Folder To Receive");
+    private static int messageCount = 0;
+
+    private static async Task Main(string[] args)
+    {
+        // fullPath=bin\Debug\net6.0\Data Folder To Receive;
+        if (!Directory.Exists(directory))
+            Directory.CreateDirectory(directory);
+
+        await using var client = new ServiceBusClient(serviceBusConnectionString);
+
+        var processorOptions = new ServiceBusProcessorOptions
+        {
+            MaxConcurrentCalls = 1,
+            AutoCompleteMessages = false
+        };
+
+        await using ServiceBusProcessor processor = client.CreateProcessor(serviceBusTopicName, serviceBusSubName, processorOptions);
+
+        processor.ProcessMessageAsync += MessageHandler;
+        processor.ProcessErrorAsync += ErrorHandler;
+
+        await processor.StartProcessingAsync();
+
+        Console.Read();
+
+        await processor.CloseAsync();
+    }
+    static async Task MessageHandler(ProcessMessageEventArgs args)
+    {
+        messageCount++;
+
+        string fullPath = Path.Combine(directory, $@"doc{messageCount}.docx");
+        if (File.Exists(fullPath))
+            File.Delete(fullPath);
+
+        byte[] pdfBytes = args.Message.Body.ToArray();
+
+        using (var fileStream = new FileStream(fullPath, FileMode.Create, FileAccess.Write))
+        {
+            fileStream.Write(pdfBytes, 0, pdfBytes.Length);
+        }
+
+        await args.CompleteMessageAsync(args.Message);
+
+        Console.WriteLine($"Message:{messageCount} received");
+
+    }
+
+    static Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        Console.WriteLine(args.Exception.ToString());
+        Console.WriteLine($"Message:{messageCount} was failed");
+        if (messageCount != 0)
+            messageCount--;
+        return Task.CompletedTask;
+    }
+}
