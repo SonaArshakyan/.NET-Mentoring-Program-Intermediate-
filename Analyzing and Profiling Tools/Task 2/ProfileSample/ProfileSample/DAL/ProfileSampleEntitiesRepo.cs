15f825bb85377b4f92bfb69534753d609ea0683d
Sona_Arshakyan <Sona_Arshakyan@EPAMYERW02F5>
2023-05-08 20:23:10 +0400

    task 2

---
+++ Analyzing and Profiling Tools/Task 2/ProfileSample/ProfileSample/DAL/ProfileSampleEntitiesRepo.cs
@@ -0,0 +1,28 @@
+﻿using ProfileSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProfileSample.DAL
+{
+    public class ProfileSampleEntitiesRepo
+    {
+        private readonly ProfileSampleEntities context;
+        public ProfileSampleEntitiesRepo()
+        {
+            context = new ProfileSampleEntities();
+        }
+        public List<ImageModel> GetImages()
+        {
+
+            var sources = context.ImgSources.
+                Take(20).Select( x => new ImageModel {
+                Name = x.Name,
+                Data = x.Data
+            });
+
+            return sources.ToList<ImageModel>();
+        }
+    }
+}
