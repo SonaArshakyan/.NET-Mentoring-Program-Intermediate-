5d9381ee036814b4ecae68c4e2e7f453a09de10d
Sona_Arshakyan <Sona_Arshakyan@EPAMYERW02F5>
2023-05-21 18:23:28 +0400

    changes for task1 (subTask1 , subTask2 and subTask3)

--- LINQ, IQueryable/Expressions.Task3.E3SQueryProvider/ExpressionToFTSRequestTranslator.cs
+++ LINQ, IQueryable/Expressions.Task3.E3SQueryProvider/ExpressionToFTSRequestTranslator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Expressions.Task3.E3SQueryProvider.Models.Request;
+using Newtonsoft.Json;
+using System;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -24,35 +26,76 @@ namespace Expressions.Task3.E3SQueryProvider
         #region protected methods
 
         protected override Expression VisitMethodCall(MethodCallExpression node)
-        {
-            if (node.Method.DeclaringType == typeof(Queryable)
-                && node.Method.Name == "Where")
-            {
-                var predicate = node.Arguments[1];
-                Visit(predicate);
+        {            
+            var nodeArg = node.Arguments[0].ToString().Replace("\"", string.Empty).Trim();
 
-                return node;
+            switch (node.Method.Name)
+            {
+                case "Where":
+                    if (node.Method.DeclaringType == typeof(Queryable))             
+                    {
+                        var predicate = node.Arguments[1];
+                        Visit(predicate);
+                        return node;
+                    }
+                    break;
+                case "StartsWith":
+                    Visit(node.Object);
+                    var startsWithConstant = Expression.Constant(nodeArg + ("*"), typeof(string));
+                    Visit(startsWithConstant);
+                    return node;
+                case "EndsWith":
+                    Visit(node.Object);
+                    var endsWithConstant = Expression.Constant(("*") + nodeArg, typeof(string));
+                    Visit(endsWithConstant);
+                    return node;
+                case "Contains":
+                    Visit(node.Object);
+                    var containsConstant = Expression.Constant(("*") + nodeArg + ("*"), typeof(string));
+                    Visit(containsConstant);
+                    return node;
             }
+
             return base.VisitMethodCall(node);
         }
 
+
+
         protected override Expression VisitBinary(BinaryExpression node)
         {
+            var leftNodeValue = node.Left;
+            var rightNodeValue = node.Right;
             switch (node.NodeType)
             {
                 case ExpressionType.Equal:
                     if (node.Left.NodeType != ExpressionType.MemberAccess)
-                        throw new NotSupportedException($"Left operand should be property or field: {node.NodeType}");
+                        leftNodeValue = node.Right;
 
                     if (node.Right.NodeType != ExpressionType.Constant)
-                        throw new NotSupportedException($"Right operand should be constant: {node.NodeType}");
+                        rightNodeValue = node.Left;
 
-                    Visit(node.Left);
-                    _resultStringBuilder.Append("(");
-                    Visit(node.Right);
-                    _resultStringBuilder.Append(")");
+                    Visit(leftNodeValue);
+                    Visit(rightNodeValue);
                     break;
 
+                case ExpressionType.AndAlso:
+                    var ftsQueryRequest = new FtsQueryRequest();
+
+                    Visit(leftNodeValue);
+                    var leftSide = _resultStringBuilder;
+                    ftsQueryRequest.Statements.Add(new Statement() { Query = leftSide.ToString() });
+                    _resultStringBuilder.Clear();
+
+                    Visit(rightNodeValue);
+                    var rightSide = _resultStringBuilder;
+                    ftsQueryRequest.Statements.Add(new Statement() { Query = rightSide.ToString() });
+                    _resultStringBuilder.Clear();
+
+                    var serializedftsQueryRequest = JsonConvert.SerializeObject(ftsQueryRequest);
+                    _resultStringBuilder.Clear();
+
+                    _resultStringBuilder.Append(serializedftsQueryRequest.ToString());
+                    break;
                 default:
                     throw new NotSupportedException($"Operation '{node.NodeType}' is not supported");
             };
@@ -69,7 +112,9 @@ namespace Expressions.Task3.E3SQueryProvider
 
         protected override Expression VisitConstant(ConstantExpression node)
         {
+            _resultStringBuilder.Append("(");
             _resultStringBuilder.Append(node.Value);
+            _resultStringBuilder.Append(")");
 
             return node;
         }
--- LINQ, IQueryable/Expressions.Task3.E3SQueryProvider/Models/Request/FTSQueryRequest.cs
+++ LINQ, IQueryable/Expressions.Task3.E3SQueryProvider/Models/Request/FTSQueryRequest.cs
@@ -9,7 +9,7 @@ namespace Expressions.Task3.E3SQueryProvider.Models.Request
         public FtsQueryRequest()
         {
             // todo: remove that
-            // Statements = new List<Statement>();
+            Statements = new List<Statement>();
             // Filters = new List<Filter>();
             // Sorting = new SortingCollection();
         }
